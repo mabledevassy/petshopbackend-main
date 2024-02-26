d6e45be306c71f29c0f3be01415b56e42089a539
AnujaSreeni <127194605+AnujaSreeni@users.noreply.github.com>
2024-02-26 16:14:55 +0530

    hi

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express=require("express")
-const cors=require("cors")
-const multer=require('multer');
-const storage=multer.memoryStorage();
-const upload=multer({storage:storage});
+const express = require("express")
+const cors = require("cors")
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 // const CategoryRouter = require('./routes/Categoryroutes')
 // const SubcateRouter = require('./Routes/Subcateroutes')
 // const db = require("./Connection/Database")
 
 
 
-const app=new express();
-const catemodel=require('./model/Categorydetails')
-const subcatemodel=require('./model/Subcategorydetails')
+const app = new express();
+const catemodel = require('./model/Categorydetails')
+const subcatemodel = require('./model/Subcategorydetails')
 const itemmodel = require("./model/Itemdetails");
-const data2model=require("./model/Loginmodel")
+const data2model = require("./model/Loginmodel")
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors());
 
@@ -26,17 +26,17 @@ app.use(cors());
 // app.use("/s", SubcateRouter)
 
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     response.send("hai")
 
 })
-app.post('/new',(request,response)=>{
+app.post('/new', (request, response) => {
     console.log(request.body)
     new catemodel(request.body).save();
     response.send("Record Successfully Saved")
 
 })
-app.post('/cnew',(request,response)=>{
+app.post('/cnew', (request, response) => {
     console.log(request.body)
     new subcatemodel(request.body).save();
     response.send("Record Successfully Saved")
@@ -47,34 +47,34 @@ app.post('/cnew',(request,response)=>{
 //     new itemmodel(request.body).save();
 //     response.send("Record Successfully Saved")
 // })
-app.get("/categoryview",async(request,response)=>{
-    var data=await catemodel.find();
+app.get("/categoryview", async (request, response) => {
+    var data = await catemodel.find();
     response.send(data);
 });
-app.get("/subview",async(request,response)=>{
-    var data=await subcatemodel.find();
+app.get("/subview", async (request, response) => {
+    var data = await subcatemodel.find();
     response.send(data);
 })
-app.get('/view',async(request,response)=>{
-    var data=await catemodel.find();
+app.get('/view', async (request, response) => {
+    var data = await catemodel.find();
     response.send(data)
 });
-app.get('/views',async(request,response)=>{
-    var data=await subcatemodel.find();
+app.get('/views', async (request, response) => {
+    var data = await subcatemodel.find();
     response.send(data)
 });
-app.get('/iview',async(request,response)=>{
-    var data=await itemmodel.find();
+app.get('/iview', async (request, response) => {
+    var data = await itemmodel.find();
     response.send(data)
 })
-app.put('/edit/:id',async(request,response)=>{
-    let id=request.params.id
-    await catemodel.findByIdAndUpdate(id,request.body)
+app.put('/edit/:id', async (request, response) => {
+    let id = request.params.id
+    await catemodel.findByIdAndUpdate(id, request.body)
     response.send("Data uploaded")
 });
-app.put('/edits/:id',async(request,response)=>{
-    let id=request.params.id
-    await subcatemodel.findByIdAndUpdate(id,request.body)
+app.put('/edits/:id', async (request, response) => {
+    let id = request.params.id
+    await subcatemodel.findByIdAndUpdate(id, request.body)
     response.send("Data uploaded")
 })
 // app.put('/editi/:id',async(request,response)=>{
@@ -82,20 +82,21 @@ app.put('/edits/:id',async(request,response)=>{
 //     await itemmodel.findByIdAndUpdate(id,request.body)
 //     response.send("Data uploaded")
 // })
-app.put('/editi/:id',async(request,response)=>{
+app.put('/editi/:id', async (request, response) => {
     console.log("ass")
-    let id=request.params.id
-    await itemmodel.findByIdAndUpdate(id,request.body)
+    let id = request.params.id
+    await itemmodel.findByIdAndUpdate(id, request.body)
     response.send("Data uploaded")
 })
-app.post('/inew',upload.single('image1'),async (request,response) => {
+app.post('/inew', upload.single('image1'), async (request, response) => {
     try {
-        const { Category,Subcategory,Description,Price } = request.body
+        const { Category, Subcategory, Description, Price } = request.body
         const newdata = new itemmodel({
-           Category,Subcategory,Description,Price,
+            Category, Subcategory, Description, Price,
             image1: {
-                data:request.file.buffer,
-                contentType: request.file.mimetype,}
+                data: request.file.buffer,
+                contentType: request.file.mimetype,
+            }
         })
         console.log(newdata);
         await newdata.save();
@@ -112,19 +113,28 @@ app.post('/Loginsearch', async (request, response) => {
     const { username, password } = request.body;
     console.log(request.body)
     try {
-        const user = await data2model.findOne({ username,password });
-      
+        const user = await data2model.findOne({ username, password });
+
         if (user) {
-          response.json({ success: true, message: 'Login successful' });
+            response.json({ success: true, message: 'Login successful' });
         }
-         else {
-          response.json({ success: false, message: 'Invalid Password and email' });
+        else {
+            response.json({ success: false, message: 'Invalid Password and email' });
         }
-      } catch (error) {
+    } catch (error) {
         response.status(500).json({ success: false, message: 'Error during login' });
-      }
-  
+    }
+
 })
-app.listen(5005, (request, response) => {
+
+app.get('/view1/:id', (req, res) => {
+    const { id } = req.params
+
+    itemmodel.findById(id).then(data => {
+        res.send(data)
+    })
+
+})
+app.listen(3005, (request, response) => {
     console.log("Port ok")
 })
